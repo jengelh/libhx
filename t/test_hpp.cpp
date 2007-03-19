da12949dd0b473a134591726c661e98d81bd26e1
Jan Engelhardt <jengelh@computergmbh.de>
2007-03-19 06:29:33 +0000

    Verify that C++ compiler works in configure.ac (used for t/)

--- t/test_hpp.cpp
+++ t/test_hpp.cpp
@@ -1,1 +1,23 @@
-#include "../libHX.h"
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <libHX.h>
+
+int main(void)
+{
+	struct HXbtree *bt;
+	const char *tmp;
+	bt = HXbtree_init(HXBT_MAP | HXBT_CKEY | HXBT_CDATA | HXBT_SCMP);
+	HXbtree_add(bt, "foo", "bar");
+	tmp = HXbtree_get<const char *>(bt, "foo");
+	printf("%s\n", tmp);
+	HXbtree_free(bt);
+
+	void *hnd = HX_dlopen("/lib/libm.so.6");
+	if(hnd != NULL) {
+		double (*cosp)(double) = HX_dlsym<double (*)(double)>(hnd, "cos");
+		printf("%f\n", (*cosp)(2.0));
+		HX_dlclose(hnd);
+	}
+	return EXIT_SUCCESS;
+}
