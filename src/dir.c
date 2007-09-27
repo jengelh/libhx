593b8ec9597ed81db6807ec2b169a2c3e62254f3
Jan Engelhardt <jengelh@computergmbh.de>
2007-09-27 20:14:44 +0000

    HX_copy_file(): only return success on EEXIST when HXF_KEEP was given

--- src/dir.c
+++ src/dir.c
@@ -132,7 +132,7 @@ EXPORT_SYMBOL int HX_copy_file(const char *src, const char *dest,
 		eax = errno;
 		close(sd);
 		errno = eax;
-		if (eax == EEXIST)
+		if (extra_flags != 0 && eax == EEXIST)
 			return 1;
 		return -errno;
 	}
