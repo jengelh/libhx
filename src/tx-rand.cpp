e7f7fb43b302549f9841a02e48c8d6866de5c318
Jan Engelhardt <jengelh@medozas.de>
2009-08-11 23:44:35 +0200

    rand: do timing tests

--- src/tx-rand.cpp
+++ src/tx-rand.cpp
@@ -3,15 +3,38 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <libHX/misc.h>
+#include "internal.h"
 
 int main(void)
 {
+	struct timespec past, now, delta;
 	unsigned int i;
 
 	for (i = 0; i < 15; ++i)
 		printf("%08x ", HX_irand(0, RAND_MAX));
 	printf("\n");
+
+	clock_gettime(CLOCK_REALTIME, &past);
+	for (i = 0; i < (1 << 20); ++i) {
+		volatile unsigned int __attribute__((unused)) t =
+			HX_irand(0, RAND_MAX);
+	}
+	clock_gettime(CLOCK_REALTIME, &now);
+	HX_diff_timespec(&delta, &now, &past);
+	printf("%% method: %ld.%06ld s\n",
+	       static_cast(long, delta.tv_sec), delta.tv_nsec / 1000);
+
+	clock_gettime(CLOCK_REALTIME, &past);
+	for (i = 0; i < (1 << 20); ++i) {
+		volatile unsigned int __attribute__((unused)) t =
+			HX_irand(0, ~0U);
+	}
+	clock_gettime(CLOCK_REALTIME, &now);
+	HX_diff_timespec(&delta, &now, &past);
+	printf("/ method: %ld.%06ld s\n",
+	       static_cast(long, delta.tv_sec), delta.tv_nsec / 1000);
 
 	return EXIT_SUCCESS;
 }
