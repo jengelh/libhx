6e56ec332e8e100553866d42ed71c2560c6162df
Jan Engelhardt <jengelh@medozas.de>
2010-08-16 11:48:56 +0200

    bitmap: use correct type when calling bpq
    We took the size of the entire bitmap (or pointer, in case it was
    accessed through such) and multiplied it by CHAR_BIT. This would
    lead to 1<<x overflowing and resulting in 0, which made
    
    - set a no-op
    - clear a no-op
    - test returning false
    
    on bits higher up.

--- src/tx-compile.cpp
+++ src/tx-compile.cpp
@@ -10,13 +10,14 @@
 
 int main(void)
 {
-	unsigned long bitmap[HXbitmap_size(unsigned long, 64)];
+	unsigned long bitmap[HXbitmap_size(unsigned long, 256)];
 
 	if (HX_init() <= 0)
 		abort();
 	printf("sizeof bitmap: %zu, array_size: %zu\n",
 	       sizeof(bitmap), ARRAY_SIZE(bitmap));
 	HXbitmap_set(bitmap, 0);
+	HXbitmap_set(bitmap, 255);
 	printf(HX_STRINGIFY(1234+2 +2) "," HX_STRINGIFY(ZZ) "\n");
 	HX_exit();
 	return EXIT_SUCCESS;
