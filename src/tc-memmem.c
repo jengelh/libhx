3d6e3efc2b7d7854243ba40e8503bbcb16f865cc
Jan Engelhardt <jengelh@inai.de>
2012-05-31 19:00:11 +0200

    tc-memmem: examine speed for internal partial substring matches

--- src/tc-memmem.c
+++ src/tc-memmem.c
@@ -10,9 +10,28 @@
 #include <libHX/misc.h>
 #include <libHX/string.h>
 
-static unsigned int size = 1048576 * 128;
+static unsigned int size = 1048576 * 64;
 static const char filler_text[] =
 	"Slhrdlu cringle tongle flonging blobbity bleep blingmangl";
+
+static void long_scan(void)
+{
+	struct timespec start, stop, delta;
+	char *filler2, *p;
+
+	filler2 = malloc(size);
+	if (filler2 == NULL)
+		abort();
+	memset(filler2, 'l', size);
+	filler2[size-2] = 'a';
+
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
+	p = HX_memmem(filler2, strlen(filler2), "al", 2);
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &stop);
+	printf("long_scan: filler2=%p p=%p\n", filler2, p);
+	HX_timespec_sub(&delta, &stop, &start);
+	printf("long_scan: " HX_TIMESPEC_FMT "\n", HX_TIMESPEC_EXP(&delta));
+}
 
 int main(void)
 {
@@ -33,6 +52,9 @@ int main(void)
 	printf("%p\n", HX_memmem(filler_text, strlen(filler_text), "nangl", 5));
 	printf("%p\n", HX_memmem(filler_text, strlen(filler_text), "angl", 4));
 	printf("%p\n", HX_memmem(filler_text, strlen(filler_text), "ngl", 3));
+	printf("%p\n", HX_memmem(filler_text, strlen(filler_text), "ngl", 3));
+
+	long_scan();
 
 	for (i = 0; i < 10; ++i) {
 		printf("Search length %u...", i);
