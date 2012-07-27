e33331c75da59641d317f6ab34e6b3d0af6be63a
Jan Engelhardt <jengelh@inai.de>
2012-07-27 21:23:59 +0200

    tests: add tests for HX_timespec_sub

--- src/tc-time.c
+++ src/tc-time.c
@@ -5,7 +5,6 @@
  *	modify it under the terms of the WTF Public License version 2 or
  *	(at your option) any later version.
  */
-#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -14,8 +13,6 @@
 #include <libHX/init.h>
 #include <libHX/misc.h>
 #include "internal.h"
-
-static unsigned int sleep_amt = 1336670;
 
 static const struct timespec pairs[] = {
 	{-1, 700000000}, {-1, 400000000}, {-1, 0},
@@ -49,12 +46,29 @@ static void test_same(void)
 	}
 	printf("\n");
 
+	for (i = 0; i < ARRAY_SIZE(pairs); ++i) {
+		r = pairs[i];
+		printf(HX_TIMESPEC_FMT " - 0 = ", HX_TIMESPEC_EXP(&r));
+		HX_timespec_sub(&r, &r, &zero);
+		printf(HX_TIMESPEC_FMT "\n", HX_TIMESPEC_EXP(&r));
+	}
+	printf("\n");
+
 	/* 2s */
 	for (i = 0; i < ARRAY_SIZE(pairs); ++i) {
 		r = pairs[i];
 		printf(HX_TIMESPEC_FMT " + " HX_TIMESPEC_FMT " = ",
 		       HX_TIMESPEC_EXP(&r), HX_TIMESPEC_EXP(&r));
 		HX_timespec_add(&r, &r, &r);
+		printf(HX_TIMESPEC_FMT "\n", HX_TIMESPEC_EXP(&r));
+	}
+	printf("\n");
+
+	for (i = 0; i < ARRAY_SIZE(pairs); ++i) {
+		r = pairs[i];
+		printf(HX_TIMESPEC_FMT " - " HX_TIMESPEC_FMT " = ",
+		       HX_TIMESPEC_EXP(&r), HX_TIMESPEC_EXP(&r));
+		HX_timespec_sub(&r, &r, &r);
 		printf(HX_TIMESPEC_FMT "\n", HX_TIMESPEC_EXP(&r));
 	}
 	printf("\n");
@@ -100,48 +114,12 @@ static void test_add(void)
 		for (b = pairs; b < pairs + ARRAY_SIZE(pairs); ++b) {
 			HX_timespec_add(&r, a, b);
 			print_op2(&r, a, "+", b);
+			HX_timespec_sub(&r, a, b);
+			print_op2(&r, a, "-", b);
+			printf("----------\n");
 		}
 	}
 	printf("\n");
-}
-
-static void zsub(void)
-{
-	static const struct timespec a = {1, 0}, b = {0, 1};
-//	HX_timespec_sub(&r, &a, &b);
-//	printf(HX_TIMESPEC_FMT " = " HX_TIMESPEC_FMT " - " HX_TIMESPEC_FMT "\n",
-}
-
-static void zsleep(void)
-{
-	struct timeval  m_past, m_future, m_delta;
-	struct timespec n_past, n_future, n_delta;
-
-	printf("µsec sleep: %u\n", sleep_amt);
-
-	clock_gettime(CLOCK_REALTIME, &n_past);
-	gettimeofday(&m_past, NULL);
-	usleep(sleep_amt);
-	clock_gettime(CLOCK_REALTIME, &n_future);
-	gettimeofday(&m_future, NULL);
-
-	HX_timeval_sub(&m_delta, &m_future, &m_past);
-	printf("µsec: " HX_TIMEVAL_FMT " -> " HX_TIMEVAL_FMT
-	       " = " HX_TIMEVAL_FMT "\n",
-	       HX_TIMEVAL_EXP(&m_past), HX_TIMEVAL_EXP(&m_future),
-	       HX_TIMEVAL_EXP(&m_delta));
-
-	HX_timespec_sub(&n_delta, &n_future, &n_past);
-	printf("nsec: " HX_TIMESPEC_FMT " -> " HX_TIMESPEC_FMT
-	       " = " HX_TIMESPEC_FMT "\n",
-	       HX_TIMESPEC_EXP(&n_past), HX_TIMESPEC_EXP(&n_future),
-	       HX_TIMESPEC_EXP(&n_delta));
-
-	HX_timespec_sub(&n_delta, &n_past, &n_future);
-	printf("ns-1: " HX_TIMESPEC_FMT " -> " HX_TIMESPEC_FMT
-	       " = " HX_TIMESPEC_FMT "\n",
-	       HX_TIMESPEC_EXP(&n_past), HX_TIMESPEC_EXP(&n_future),
-	       HX_TIMESPEC_EXP(&n_delta));
 }
 
 int main(void)
@@ -152,7 +130,6 @@ int main(void)
 	test_same();
 	test_neg();
 	test_add();
-	zsleep();
 	HX_exit();
 	return EXIT_SUCCESS;
 }
