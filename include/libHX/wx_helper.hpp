21f01dc31c0e82b31bb94f1b29a489d0a431ad86
Jan Engelhardt <jengelh@inai.de>
2013-11-22 13:13:12 +0100

    wx_helper: remove wxCFF
    wxCFF is the same as wxDEFAULT_FRAME_STYLE and redundant.

--- include/libHX/wx_helper.hpp
+++ include/libHX/wx_helper.hpp
@@ -10,11 +10,8 @@
 /* Convert from wxString to UTF-8; limited validity */
 #define wxtu8(s)	static_cast<const char *>((s).ToUTF8())
 
-/* Common frame flags */
-#define wxCFF     (wxCAPTION | wxRESIZE_BORDER | wxSYSTEM_MENU | \
-                  wxCLOSE_BOX | wxMAXIMIZE_BOX | wxMINIMIZE_BOX)
 /* Common dialog flags */
-#define wxCDF     (wxCFF | wxFRAME_NO_TASKBAR)
+#define wxCDF     (wxDEFAULT_FRAME_STYLE | wxFRAME_NO_TASKBAR)
 #define wxACV     wxALIGN_CENTER_VERTICAL
 #define wxDPOS    wxDefaultPosition
 #define wxDSIZE   wxDefaultSize
