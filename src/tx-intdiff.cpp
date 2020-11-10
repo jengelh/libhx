2ff8357139089b6ae1a533f3f342952e4130f427
Jan Engelhardt <jengelh@inai.de>
2020-11-10 23:29:07 +0100

    build: add tx-intdiff.cpp for set_comm

---
+++ src/tx-intdiff.cpp
@@ -0,0 +1,23 @@
+#include <iterator>
+#include <vector>
+#include <libHX/intdiff.hpp>
+int main()
+{
+	std::vector<int> a{1, 2, 5};
+	std::vector<long> b{2, 3, 4, 5};
+	std::vector<float> comm, left, right;
+
+	HX::set_intersect_diff(a.cbegin(), a.cend(), b.cbegin(), b.cend(),
+		std::back_inserter(comm), std::back_inserter(left),
+		std::back_inserter(right));
+	for (auto e : comm)
+		printf("%f,", e);
+	printf("\n");
+	for (auto e : left)
+		printf("%f,", e);
+	printf("\n");
+	for (auto e : right)
+		printf("%f,", e);
+	printf("\n");
+	return 0;
+}
