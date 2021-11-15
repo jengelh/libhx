af099690adcb83f64e24928f417ca198dc48fa38
Jan Engelhardt <jengelh@inai.de>
2021-11-15 19:59:38 +0100

    time: use 64-bit arithmetic
    Looks like doing 64-bit arith is better for HX_timespec_add
    after all.
    
    gcc-7 -Os
    arith          x86_64 x86
    i32.i32 arith  179    150
    i64 arith      93     94

--- src/time.c
+++ src/time.c
@@ -6,6 +6,7 @@
  *	General Public License as published by the Free Software Foundation;
  *	either version 2.1 or (at your option) any later version.
  */
+#include <stdint.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <stdbool.h>
@@ -39,38 +40,13 @@ HX_timespec_neg(struct timespec *r, const struct timespec *a)
 EXPORT_SYMBOL struct timespec *HX_timespec_add(struct timespec *r,
     const struct timespec *a, const struct timespec *b)
 {
-	/*
-	 * Split the value represented by the struct into two
-	 * independent values that can be added individually.
-	 */
-	long nsec[2];
-	nsec[0] = (a->tv_sec < 0) ? -a->tv_nsec : a->tv_nsec;
-	nsec[1] = (b->tv_sec < 0) ? -b->tv_nsec : b->tv_nsec;
-
-	r->tv_sec  = a->tv_sec + b->tv_sec;
-	r->tv_nsec = nsec[0] + nsec[1];
-	if (r->tv_nsec >= NANOSECOND) {
-		++r->tv_sec;
-		r->tv_nsec -= NANOSECOND;
-	} else if (r->tv_nsec <= -NANOSECOND) {
-		--r->tv_sec;
-		r->tv_nsec += NANOSECOND;
-	}
-
-	/* Combine again */
-	if (r->tv_sec < 0) {
-		if (r->tv_nsec < 0) {
-			r->tv_nsec = -r->tv_nsec;
-		} else if (r->tv_nsec > 0) {
-			if (++r->tv_sec == 0)
-				r->tv_nsec = -NANOSECOND + r->tv_nsec;
-			else
-				r->tv_nsec = NANOSECOND - r->tv_nsec;
-		}
-	} else if (r->tv_sec > 0 && r->tv_nsec < 0) {
-		--r->tv_sec;
-		r->tv_nsec = NANOSECOND + r->tv_nsec;
-	}
+	long aa = a->tv_sec * NANOSECOND_LL +
+	          ((a->tv_sec >= 0) ? a->tv_nsec : -a->tv_nsec);
+	long bb = b->tv_sec * NANOSECOND_LL +
+	          ((b->tv_sec >= 0) ? b->tv_nsec : -b->tv_nsec);
+	long rr = aa + bb;
+	r->tv_sec  = rr / NANOSECOND;
+	r->tv_nsec = ((r->tv_sec < 0) ? -rr : rr) % NANOSECOND;
 	return r;
 }
 
